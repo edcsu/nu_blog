92ded36337bd9dace93a61b1e32b478b5b26a5ed
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-21 16:35:29 +0300

    refactor: reuse method to find a blog post

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,12 @@
 class BlogPostsController < ApplicationController
+
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -24,11 +25,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
-    end
+  end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if(@blog_post.update(blog_post_params))
       redirect_to @blog_post
     else
@@ -37,7 +36,6 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
@@ -45,5 +43,11 @@ class BlogPostsController < ApplicationController
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
