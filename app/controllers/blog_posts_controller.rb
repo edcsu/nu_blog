764a0201012254c9e578f6e3b460728fbc22422c
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-25 01:02:49 +0300

    refactor: sort blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,7 +2,7 @@ class BlogPostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
   def index
-    @blog_posts = BlogPost.published
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
