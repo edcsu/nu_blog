5c2568cbeb2edc5d8679464654771e67f2d03270
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-27 20:38:53 +0300

    tests: add published blogpost tests

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -12,4 +12,16 @@ class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns false for a scheduled blog post" do
     refute BlogPost.new(published_at: 1.year.from_now).draft?
   end
+
+  test "published? returns true for a published blog post" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? returns false for a draft blog post" do
+    refute BlogPost.new(published_at: nil).published?
+  end
+
+  test "published? returns false for a scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).published?
+  end
 end
